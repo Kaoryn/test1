18ffba57fd7ca37770cc79b074d9c505a2769aaf
forziking <sdjglk@mail.ru>
2023-02-01 11:32:01 +0300

    test

---
+++ Page1.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyFirstApp
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Label1.Content = "Привет";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Label1.Content = "До свидания";
+        }
+    }
+}
